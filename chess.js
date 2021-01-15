0cbabc694171405920c89855a1d9595b7dad78bc
nooralam15 <55167857+nooralam15@users.noreply.github.com>
2021-01-15 12:05:15 -0700

    Add files via upload

--- chess.js
+++ chess.js
@@ -1,6 +1,5 @@
 
-//create the chess board
-
+//create the chess board 
 function createGridArray() {
     //create and return a grid array
     return [["b2", "b3", "b4", "b5", "b6", "b4", "b3", "b2"],
@@ -13,32 +12,30 @@ function createGridArray() {
             ["w2", "w3", "w4", "w5", "w6", "w4", "w3", "w2"]]
 }
 
-
+//this function will create an array of divs that will later be used to efficiently update the gameboard
 function createDivArray(grid) {
+    //creatign a temporary array that will store the array of divs
     let temp = []
     for (let row = 0; row < num_row; row++) {
         let tempRow = []
         if (row % 2 == 0) {
             for (let col = 0; col < num_col; col++) {   
                 if (col % 2 == 0) {
-                    tempRow.push(cellDesign(0,0))
+                    tempRow.push(cellMechanics(row,col, 0,0))
                 }
                 else {
-                    tempRow.push(cellDesign(0,1))
+                    tempRow.push(cellMechanics(row, col, 0,1))
                 }
             }
         }
         else {
             for (let col = 0; col < num_col; col++) {
-                //let gridData = grid[row][col]
-                //pieceDisplay(gridData)
-                
                 if (col % 2 == 0) {
-                    tempRow.push(cellDesign(1,0))
+                    tempRow.push(cellMechanics(row, col, 1,0))
 
                 }
                 else {
-                    tempRow.push(cellDesign(1,1))
+                    tempRow.push(cellMechanics(row, col, 1,1))
                 }
             }
         }
@@ -47,30 +44,45 @@ function createDivArray(grid) {
     return temp
 }
 
-//function displays the Chess pattern
-function cellDesign (rowVal, colVal) {
+//function creates a div element and sets up various properties for each cell
+function cellMechanics (rowVal, colVal, rowDes, colDes) {
+    //create a div element for each element in the 2d grid
     let divEl = document.createElement("div")
-    if (rowVal == 0 && colVal == 0) {
-        divEl.classList.add("lightBrown")
-        //divEl.innerHTML = `<img src = images/whitePawn.png width="80px" height="75px">`
-        document.getElementById("container").append(divEl)
-    }
-    else if (rowVal == 0 && colVal == 1) {
-        divEl.classList.add("darkBrown")
-        
-        document.getElementById("container").append(divEl) 
-    }
-    else if (rowVal == 1 && colVal == 0) {
-        divEl.classList.add("darkBrown")
-        document.getElementById("container").append(divEl)
-    }
-    else {
-        divEl.classList.add("lightBrown")
-        document.getElementById("container").append(divEl)
-    }
+
+    //add class that will give each cell its common design
+    divEl.classList.add("board")
+
+    //add en eventListener to each divEl
+    divEl.addEventListener("click", cellClicked)
+
+    //add dataset values for rows and columns
+    divEl.dataset.row = rowVal
+    divEl.dataset.col = colVal
+
+    //calls funnctiont that will display chess pattern
+    displayPattern(rowDes, colDes, divEl)
+    
     return divEl
 }
 
+//function displays chess board pattern 
+function displayPattern (rowDes, colDes, divEl) {
+    if (rowDes == 0 && colDes == 1 || rowDes == 1 && colDes == 0) {
+        divEl.classList.add("darkBrown")
+    }
+    else {
+        divEl.classList.add("lightBrown")
+    }
+    document.getElementById("container").append(divEl)
+
+}
+
+function cellClicked(event) {
+    event.target.classList.add("selectedPiece")
+    console.log(event.target)
+}
+
+//this function goes through the chessBoard array and will update the board to display the correct piece image
 function updateBoard() {
     for (let row = 0; row < num_row; row++) {
         for(let col = 0; col < num_col; col++) {
@@ -114,6 +126,7 @@ function updateBoard() {
     }
     
 }
+
 
 
 
